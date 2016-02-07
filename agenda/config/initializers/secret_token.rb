df9d56194c9338f4b6aeba1fe60c0f80fd3d76e3
wdmeida <wdmeida@gmail.com>
2016-02-07 14:53:54 -0200

    Arquivos gerados inicialmente

---
+++ agenda/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agenda::Application.config.secret_key_base = 'c8da5cd982e86a5cdeb03f6dbd27d756497c46adb06cbb39616d50317b76845e6bb77121c1380c6944bdc688d2bd37257cebd1ec788532e526a20ce23ef15c22'
